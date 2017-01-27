0829ef04229fd7b5f3db9100bef46291841e2492
Victor Presumido <victor.presumido@gmail.com>
2017-01-27 16:26:39 -0200

    Refactor spec

--- spec/models/twitter_query_spec.rb
+++ spec/models/twitter_query_spec.rb
@@ -5,10 +5,9 @@ RSpec.describe TwitterQuery, type: :model do
   it "removes offensive words" do
     offensive_words = TwitterQuery::OFFENSIVE_WORDS_DICT.take(3)
     tweet_text = Faker::Lorem.paragraph(2)
-    offensive_words.each do |word|
-      tweet_text.insert(rand(0..tweet_text.length),word)
-    end
-    tweet_text+=" #{offensive_words[1]}"
+    tweet_text+="#{offensive_words[0] }"
+    tweet_text.insert(rand(0..tweet_text.length),offensive_words[1])
+    tweet_text+=" #{offensive_words[2]}"
     expect(TwitterQuery.clean_tweet(tweet_text).split(" ")).not_to include(offensive_words)
   end
 end
